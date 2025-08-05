1bef76c62243dce050a208486d1d3e3a261006db
abdo <dealkalr@gmail.com>
2025-08-05 17:13:32 +0100

    check map 20%

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 13:13:00 by abdo              #+#    #+#             */
-/*   Updated: 2025/08/05 15:57:15 by abdo             ###   ########.fr       */
+/*   Updated: 2025/08/05 17:08:26 by abdo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,29 +40,25 @@ char *read_line(char *s)
         if (rbyt  == 0)
             break;
         str = ft_strjoin(str, buf);
+        ft_memset(buf,0,10);
     }
     close(fd);
     return str;
 }
-// char **fill_map(char *s)
-// {
-  
-//     char **p;
-//     char *str;
-//     int i = 0;
-//     char buf[100];
-//     while (*s && *s != '\n')
-//         i++;
-//     p = malloc(sizeof(char*) * (i + 1));
-//     str = read_line(s);
-    
-    
-    
-// }
+char **fill_map(char *s)
+{
+    char **p;
+    char *str;
+    if (!*s)
+        return NULL;
+    str = read_line(s); 
+    p = ft_split(str, '\n');
+    return p;
+}
 int main(int argc, char **argv)
 {
     int fd;
-    char *map;
+    char **map;
     if (argc != 2)
         return 1;
     if (check_arg(argv[1]) == -1)
@@ -71,7 +67,13 @@ int main(int argc, char **argv)
         return 1;
     }
         
-    map =read_line(argv[1]);
-    printf("%s\n", map);
+    map =fill_map(argv[1]);
+    if (!map)
+        return 1;
+    for (int i = 0; map[i] != NULL; i++)
+    {
+        printf("%s\n", map[i]);
+    }
+    
     return 0;
 }
