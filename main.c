890a436b6332d8337daa63ce0d3bd3adf4f870d9
abdo <dealkalr@gmail.com>
2025-08-05 16:37:42 +0100

    starting parsing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 13:13:00 by abdo              #+#    #+#             */
-/*   Updated: 2025/08/05 15:13:08 by abdo             ###   ########.fr       */
+/*   Updated: 2025/08/05 15:57:15 by abdo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include "./libft/libft.h"
 
 int check_arg(char *s)
 {
@@ -25,42 +27,51 @@ int check_arg(char *s)
 }
 char *read_line(char *s)
 {
-    char buf[100];
+    char buf[10];
     int fd;
-    int rbyt;
-    
+    int rbyt = 1;
+    char *str = NULL;
+    if (!s)
+        return NULL;
     fd = open(s,O_RDONLY,640);
-    while ()
+    while (rbyt != 0)
     {
-        /* code */
+        rbyt = read(fd,buf, 10);
+        if (rbyt  == 0)
+            break;
+        str = ft_strjoin(str, buf);
     }
-    
-    
-    
+    close(fd);
+    return str;
 }
-char **fill_map(char *s)
-{
+// char **fill_map(char *s)
+// {
   
-    char **p;
-    char *str;
-    int i = 0;
-    char buf[100];
-    while (*s && *s != '\n')
-        i++;
-    p = malloc(sizeof(char*) * (i + 1));
-    str = read_line(s);
+//     char **p;
+//     char *str;
+//     int i = 0;
+//     char buf[100];
+//     while (*s && *s != '\n')
+//         i++;
+//     p = malloc(sizeof(char*) * (i + 1));
+//     str = read_line(s);
     
     
     
-}
+// }
 int main(int argc, char **argv)
 {
     int fd;
-    char **map;
+    char *map;
     if (argc != 2)
         return 1;
     if (check_arg(argv[1]) == -1)
+    {
+        printf("Error\n");
         return 1;
-    map =fill_map(argv[1]);
+    }
+        
+    map =read_line(argv[1]);
+    printf("%s\n", map);
     return 0;
 }
