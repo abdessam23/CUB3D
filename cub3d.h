4d7dba5169b646f6c6ec486ac1536fe1f5711905
abdo <dealkalr@gmail.com>
2025-08-09 18:48:24 +0100

    ok

---
+++ cub3d.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/09 15:04:15 by abdo              #+#    #+#             */
+/*   Updated: 2025/08/09 15:04:51 by abdo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef CUB3D_H
+# define CUB3D_H
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "./libft/libft.h"
+
+
+#endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 13:13:00 by abdo              #+#    #+#             */
-/*   Updated: 2025/08/07 13:13:36 by abdo             ###   ########.fr       */
+/*   Updated: 2025/08/09 14:48:00 by abdo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int check_arg(char *s)
 {
     char *str;
     str = ft_strrchr(s,'.');
-    if (strcmp(str, ".cub") == 0)
+    if (strcmp(str, ".cub") == 0) //should use our strcmp
         return 1;
     return -1;
 }
@@ -67,76 +67,115 @@ char *read_line(char *s)
     
     return str;
 }
-char **fill_map(char *s)
+
+int iswhitespace(char *s)
 {
-    char **p;
-    char *str;
-    if (!*s)
-        return NULL;
- 
-    str = read_line(s); 
-    p = ft_split(str, '\n');
-    return p;
-}
-int checkup_down(char *s)
-{
-    int i = 0;
-    
-    while (s[i])
+    while (*s)
     {
-        if (s[i] != ' ' &&  s[i] != '1')
-            return 0;
-        i++;
-    }
-    return 1; 
-}
-int ft_side(char *s)
-{
-    int i =0;
-    if (!s || !*s)
+       if(*s != ' ')
         return 0;
-    while (s[i] == ' ')
-        i++;
-    if (s[i] != '1')
-        return 0;
-    while (s[i])
-    {
-        if(s[i + 1] == '\0' && s[i] == '1')
-            return 1;
-        i++;
-    }
-    return 0;
-}
-int check_side(char **str)
-{
-    int i = 1;
-    int  j;
-    while (str[i])
-    {
-        if (!ft_side(str[i]))
-            return 0;
-        i++;
+        s++;
     }
     return 1;
 }
-int check_walls(char **map)
+
+int fill_map(char *s)
 {
-    char **str;
-    str = map + 6;
-    int i = 0;
-    if (!map || !*map)
+    // char **p;
+    char *str;
+    int count = 0;
+    int line;
+    if (!*s)
         return 0;
-    while (str[i] != NULL)
-        i++;
-    if (!checkup_down(str[0]) || !checkup_down(str[i - 1])  || !check_side(str))
-        return 0;
-    return 1;        
+    
+    str = read_line(s);
+    printf("%s\n",str);
+    while (*str)
+    {
+       while (*str && *str != '\n')
+            str++;
+        line = iswhitespace(str);
+        if (!line)
+            count = 1;
+        if (line && count)
+        {
+            char *look = str;
+            while (*look)
+            {
+                if (*look != ' ' && *look != '\n')
+                    return 1;
+                look++;
+            }
+        } 
+        if (*str == '\n')
+            str++;     
+    }
+    
+    // int i = 0; 
+    // p = ft_split(str, '\n');
+    // return p;
+    return 0;
 }
+// int checkup_down(char *s)
+// {
+//     int i = 0;
+    
+//     while (s[i])
+//     {
+//         if (s[i] != ' ' &&  s[i] != '1')
+//             return 0;
+//         i++;
+//     }
+//     return 1; 
+// }
+// int ft_side(char *s)
+// {
+//     int i =0;
+//     if (!s || !*s)
+//         return 0;
+//     while (s[i] == ' ')
+//         i++;
+//     if (s[i] != '1')
+//         return 0;
+//     while (s[i])
+//     {
+//         if(s[i + 1] == '\0' && s[i] == '1')
+//             return 1;
+//         i++;
+//     }
+//     return 0;
+// }
+// int check_side(char **str)
+// {
+//     int i = 1;
+//     int  j;
+//     while (str[i])
+//     {
+//         if (!ft_side(str[i]))
+//             return 0;
+//         i++;
+//     }
+//     return 1;
+// }
+// int check_walls(char **map)
+// {
+//     char **str;
+//     str = map + 6;
+//     int i = 0;
+//     if (!map || !*map)
+//         return 0;
+//     while (str[i] != NULL)
+//         i++;
+//     if (!checkup_down(str[0]) || !checkup_down(str[i - 1])  || !check_side(str))
+//         return 0;
+//     return 1;        
+// }
 int main(int argc, char **argv)
 {
     int fd;
-    char **map;
+    int map;
     int i;
+    int c;
     if (argc != 2)
         return 1;
     if (check_arg(argv[1]) == -1)
@@ -145,24 +184,29 @@ int main(int argc, char **argv)
         return 1;
     }
     map =fill_map(argv[1]);
-    if (!map)
-        return 1;
-    for (i = 0; map[i] != NULL; i++)
-    {
-        if (i < 6)
-        {
-            if (!check_ident(map[i]))
-            {
-                printf("Error : the order does'nt corret!");
-                return 1;
-            }
-        }
-    }
-    if (!check_walls(map))
-    {
-        printf("walls issue");
-        return 1;
-    }
+    // if (!map)
+    //     return 1;
+    // for (i = 0; map[i] != NULL; i++)
+    // {
+    //     if (check_ident(map[i]))
+    //     {
+    //         if (!check_ident(map[i]))
+    //         {
+    //             // printf("Error : the order does'nt correct!");
+    //             return 1;
+    //         }
+    //     }
+    //     // printf("%s\n",map[i]);
+    // }
+    // if (!check_walls(map))
+    // {
+    //     printf("walls issue");
+    //     return 1;
+    // }
+    if (map)
+        printf("there are space inside  map\n");
+    else
+        printf("ok!\n");
     
     return 0;
 }
