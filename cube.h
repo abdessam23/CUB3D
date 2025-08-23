32df37e825fcb96d9e3791072d6fa9159f5ca949
Abdellahsyani <syaniabdos@gmail.com>
2025-08-23 12:10:10 +0100

    add

--- cube.h
+++ cube.h
@@ -6,8 +6,11 @@
 #include <math.h>
 #include "mlx.h"
 
+#define M_PI	3.14159265358979323846
 #define WIN_WIDTH 1000
 #define	WIN_HEIGHT 800
+#define	VIEW 60
+#define V_RAD (VIEW * M_PI / 180.0f)
 
 typedef struct s_game
 {
@@ -30,5 +33,8 @@ typedef struct s_player
 	float	y;
 	float	dirx;
 	float	diry;
+	float	p_view;
+	float	angle;
+
 }	t_player;
 #endif
--- start_game.c
+++ start_game.c
@@ -12,13 +12,39 @@
 
 #include "cube.h"
 
+void	get_player_pos(t_player *player)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (map[x])
+	{
+		y = 0;
+		while (map[y])
+		{
+			if (map[x][y] == 'P')
+			{
+				player->x = map[x];
+				player->y = map[y];
+			}
+			y++;
+		}
+		x++;
+	}
+}
+
 int	init_cube(void)
 {
 	t_game	gm;
 	t_data	img;
+	t_player	player;
 
+	player.view = V_RAD;
+	player.angle = 0;
 	gm.mlx = mlx_init();
 	gm.mlx_window = mlx_new_window(gm.mlx, MAX_WIDTH, MAX_HEIGHT, "cube3d");
 	img.img = mlx_new_image(gm.mlx, 800, 1000);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	get_player_pos(&player);
 }
