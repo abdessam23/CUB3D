9b4c0802dcaae67eac85089f77413fbf4d08991f
abdo <dealkalr@gmail.com>
2025-08-27 11:46:47 +0100

    texX

--- run_game.c
+++ run_game.c
@@ -115,6 +115,7 @@ void run_game(t_player player)
             else
                 wallX = player.playerX + wallp * raydiX;
             wallX -=floor(wallX);
+            int texX = (int) wallX * 64;
         }
     }
     
