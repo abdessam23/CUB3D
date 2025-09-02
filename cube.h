0cff7aebe04a13c0a883388339b537d778e5add5
Abdellahsyani <syaniabdos@gmail.com>
2025-09-02 09:31:54 +0100

    add code

--- cube.h
+++ cube.h
@@ -4,20 +4,30 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
-#include "./libft/libft.h"
+#include "../lib_ft/libft.h"
 #include "mlx.h"
 
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+#define MAX_WIDTH 800
+#define MAX_HEIGHT 600
+
+# define KEY_W 119
+# define KEY_A 97
+# define KEY_S 115
+# define KEY_D 100
+# define KEY_ESC 65307
+# define KEY_LEFT 65361
+# define KEY_RIGHT 65363
+
+// Movement speeds
+# define MOVE_SPEED 0.5
+# define ROT_SPEED 0.05
+
 #define PI	3.14159265358979323846
-#define WIN_WIDTH 1000
-#define	WIN_HEIGHT 800
 #define	FOV 66.0
 #define V_RAD (VIEW * PI / 180.0)
 
-typedef struct s_game
-{
-	void	*mlx;
-	void	*mlx_window;
-}	t_game;
 
 typedef struct	s_data
 {
@@ -57,4 +67,35 @@ typedef struct s_player
 	int dirY;
 	int	texX;
 }	t_player;
+
+typedef struct s_game
+{
+	void	*mlx;
+	void	*mlx_window;
+	t_data	img;
+	t_player *player;
+	int         keys[65536];
+}	t_game;
+
+int     *direction(char c);
+void    set_plane(t_player *player);
+void    calc_step(t_player *player);
+void    DDA_algo(t_player *player);
+void    wall_calc(t_player *player);
+void    prep_calcs(t_player *player, int i);
+void    put_pixel(t_data *img, int x, int y, int color);
+void    draw_wall_column(t_game *game, t_player *player, int column);
+void    run_game(t_player *player, t_game *game);
+void    init_player_direction(t_player *player);
+void    init_player(t_player *player);
+int     game_loop(t_game *game);
+void    render_frame(t_game *game);
+int     close_window(t_game *game);
+char    **create_test_map(void);
+int     init_cube(void);
+int     key_press(int keycode, t_game *game);
+int     key_release(int keycode, t_game *game);
+void    rotate_player(t_player *player, float angle);
+void    handle_movement(t_game *game);
+
 #endif
--- raycast/run_game.c
+++ raycast/run_game.c
@@ -1,4 +1,16 @@
-#include "cub3d.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   run_game.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/01 14:22:27 by asyani            #+#    #+#             */
+/*   Updated: 2025/09/02 09:30:46 by asyani           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cube.h"
 
 int *direction(char c)
 {
@@ -26,26 +38,25 @@ int *direction(char c)
 
 void	set_plane(t_player *player)
 {
-	// Set plane vector perpendicular to direction
 	if (player->direction == 'N')
-	{
-		player->planeX = 0;
-		player->planeY = -0.66;
-	}
-	else if (player->direction == 'S')
-	{
-		player->planeX = 0;
-		player->planeY = 0.66;
-	}
-	else if (player->direction == 'E')
 	{
 		player->planeX = 0.66;
 		player->planeY = 0;
 	}
-	else if (player->direction == 'W')
+	else if (player->direction == 'S')
 	{
 		player->planeX = -0.66;
 		player->planeY = 0;
+	}
+	else if (player->direction == 'E')
+	{
+		player->planeX = 0;
+		player->planeY = 0.66;
+	}
+	else if (player->direction == 'W')
+	{
+		player->planeX = 0;
+		player->planeY = -0.66;
 	}
 }
 
@@ -86,7 +97,7 @@ void	DDA_algo(t_player *player)
 			player->side = 0;
 		}
 		else
-		{
+	{
 			player->dsidY += player->dy;
 			player->mapY += player->stepY;
 			player->side = 1;
@@ -96,6 +107,40 @@ void	DDA_algo(t_player *player)
 	}
 }
 
+
+void	prep_calcs(t_player *player, int i)
+{
+	player->mapX = (int)player->playerX;
+	player->mapY = (int)player->playerY;
+
+	printf("mapx: %d mapy: %d\n", player->mapX, player->mapY);
+	player->hit = 0;
+	player->cameraX = 2 * (float)i / WIN_WIDTH - 1;
+	player->raydiX = player->dirX + player->planeX * player->cameraX;
+	player->raydiY = player->dirY + player->planeY * player->cameraX;
+
+	// Calculate delta distances
+	if (player->raydiX == 0)
+		player->dx = 1e30;
+	else
+		player->dx = fabs(1 / player->raydiX);
+
+	if (player->raydiY == 0)
+		player->dy = 1e30;
+	else
+		player->dy = fabs(1 / player->raydiY);
+}
+
+void	put_pixel(t_data *img, int x, int y, int color)
+{
+	char	*dst;
+
+	if (x >= 0 && x < WIN_WIDTH && y >= 0 && y < WIN_HEIGHT)
+	{
+		dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+		*(unsigned int*)dst = color;
+	}
+}
 
 void	wall_calc(t_player *player)
 {
@@ -123,67 +168,257 @@ void	wall_calc(t_player *player)
 	player->texX = (int)(player->wallX * 64);
 }
 
-void	prep_calcs(t_player *player)
+void	draw_wall_column(t_game *game, t_player *player, int column)
 {
-	player->mapX = (int)player->playerX;
-	player->mapY = (int)player->playerY;
+	// Calculate line height and draw positions
+	int line_height = (int)(WIN_HEIGHT / player->wallp);
+	int start_draw = WIN_HEIGHT / 2 - line_height / 2;
+	int end_draw = WIN_HEIGHT / 2 + line_height / 2;
 
-	player->hit = 0;
-	player->cameraX = 2 * (float)i / width - 1;
-	player->raydiX = player->dirX + player->planeX * player->cameraX;
-	player->raydiY = player->dirY + player->planeY * player->cameraX;
+	if (start_draw < 0) start_draw = 0;
+	if (end_draw >= WIN_HEIGHT) end_draw = WIN_HEIGHT - 1;
 
-	// Calculate delta distances
-	if (player->raydiX == 0)
-		player->dx = 1e30;
+	// Choose wall color based on side (simple version)
+	int color;
+	if (player->side == 0)
+		color = 0xFF0000;  // Red for North/South walls
 	else
-		player->dx = fabs(1 / player->raydiX);
+		color = 0x00FF00;  // Green for East/West walls
 
-	if (player->raydiY == 0)
-		player->dy = 1e30;
-	else
-		player->dy = fabs(1 / player->raydiY);
+	// Draw ceiling (above wall)
+	for (int y = 0; y < start_draw; y++)
+		put_pixel(&game->img, column, y, 0x87CEEB);  // Sky blue
+
+	// Draw wall
+	for (int y = start_draw; y < end_draw; y++)
+		put_pixel(&game->img, column, y, color);
+
+	// Draw floor (below wall)
+	for (int y = end_draw; y < WIN_HEIGHT; y++)
+		put_pixel(&game->img, column, y, 0x8B4513);  // Brown floor
 }
 
-void	run_game(t_player *player)
+
+void	run_game(t_player *player, t_game *game)
 {
-	player->side = 0;
-	player->hit = 0;
-	player->planeX = 0;
-	player->planeY  = 0.66;
-	player->p = direction(player->direction);
-	player->dirX = p[0];
-	player->dirY = p[1];
-	free(player->p);
-
-
-	set_plane(player);
 	// Cast rays for each column
 	for (int i = 0; i < WIN_WIDTH; i++)
 	{
-		prep_calcs(player);
+		prep_calcs(player, i);
 		calc_step(player);
 		DDA_algo(player);
 		wall_calc(player);
-		// draw the vertical line from startdraw to enddraw
-		for (int i = startdraw; i < enddraw; i++)
-			start_draw(i, startdraw, enddraw, texX, side);
+		draw_wall_column(game, player, i);
 	}
 }
 
-int	init_cube(void)
+
+int key_press(int keycode, t_game *game)
 {
-	t_game	gm;
-	t_data	img;
-	t_player	player;
-
-	player.view = V_RAD;
-	player.angle = 0;
-	gm.mlx = mlx_init();
-	gm.mlx_window = mlx_new_window(gm.mlx, MAX_WIDTH, MAX_HEIGHT, "cube3d");
-	img.img = mlx_new_image(gm.mlx, 800, 1000);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-
-	mlx_loop(mlx);
+	if (keycode == KEY_ESC)
+		close_window(game);
+	if (keycode < 65536)
+		game->keys[keycode] = 1;
 	return (0);
+}
+
+int key_release(int keycode, t_game *game)
+{
+	if (keycode < 65536)
+		game->keys[keycode] = 0;
+	return (0);
+}
+
+void rotate_player(t_player *player, float angle)
+{
+	// Save original values
+	float oldDirX = player->dirX;
+	float oldPlaneX = player->planeX;
+
+	// Rotate direction vector
+	player->dirX = player->dirX * cos(angle) - player->dirY * sin(angle);
+	player->dirY = oldDirX * sin(angle) + player->dirY * cos(angle);
+
+	// Rotate plane vector
+	player->planeX = player->planeX * cos(angle) - player->planeY * sin(angle);
+	player->planeY = oldPlaneX * sin(angle) + player->planeY * cos(angle);
+}
+
+void handle_movement(t_game *game)
+{
+	t_player *player = game->player;
+
+	// Rotation (Left/Right arrows or A/D keys)
+	if (game->keys[KEY_LEFT] || game->keys[KEY_A])
+		rotate_player(player, -ROT_SPEED);  // Turn left
+
+	if (game->keys[KEY_RIGHT] || game->keys[KEY_D])
+		rotate_player(player, ROT_SPEED);   // Turn right
+
+	// Forward movement (W key)
+	if (game->keys[KEY_W])
+	{
+		float newX = player->playerX + player->dirX * MOVE_SPEED;
+		float newY = player->playerY + player->dirY * MOVE_SPEED;
+
+		// Check collision with walls
+		if (player->map[(int)newY][(int)player->playerX] != '1')
+			player->playerY = newY;
+		if (player->map[(int)player->playerY][(int)newX] != '1')
+			player->playerX = newX;
+	}
+
+	// Backward movement (S key)
+	if (game->keys[KEY_S])
+	{
+		float newX = player->playerX - player->dirX * MOVE_SPEED;
+		float newY = player->playerY - player->dirY * MOVE_SPEED;
+
+		// Check collision with walls
+		if (player->map[(int)newY][(int)player->playerX] != '1')
+			player->playerY = newY;
+		if (player->map[(int)player->playerY][(int)newX] != '1')
+			player->playerX = newX;
+	}
+}
+
+void	init_player_direction(t_player *player)
+{
+	// Get initial direction
+	printf("direction: %c\n", player->direction);
+	int *p = direction(player->direction);
+	player->dirX = p[0];
+	player->dirY = p[1];
+	printf("dirx: %d diry: %d\n", player->dirX, player->dirY);
+	printf("playerx: %f playery: %f\n", player->playerX, player->playerY);
+	free(p);
+
+	// Set plane vector
+	set_plane(player);
+}
+
+void	init_player(t_player *player)
+{
+	player->map = create_test_map();
+	for (int x = 0; x < 10; x++)
+	{
+		for (int y = 0; y < 10; y++)
+		{
+			if (player->map[x][y] == 'N')
+       			{
+				printf("find N in %d,%d\n", x, y);
+				player->direction = 'N';
+				player->playerX = (float)x;
+				player->playerY = (float)y;
+			}
+			else if (player->map[x][y] == 'W')
+       			{
+				printf("find W in %d,%d\n", x, y);
+				player->playerX = (float)x;
+				player->playerY = (float)y;
+				player->direction = 'W';
+			}
+			else if (player->map[x][y] == 'E')
+       			{
+				printf("find E in %d,%d\n", x, y);
+				player->playerX = (float)x;
+				player->playerY = (float)y;
+				player->direction = 'E';
+			}
+       			else if (player->map[x][y] == 'S')
+			{
+				printf("find S in %d,%d\n", x, y);
+				player->direction = 'S';
+				player->playerX = (float)x;
+				player->playerY = (float)y;
+			}
+		}
+	}
+	init_player_direction(player);
+}
+
+int game_loop(t_game *game)
+{
+	handle_movement(game);
+	render_frame(game);
+	return (0);
+}
+
+void	render_frame(t_game *game)
+{
+	run_game(game->player, game);
+	mlx_put_image_to_window(game->mlx, game->mlx_window, game->img.img, 0, 0);
+}
+
+int	close_window(t_game *game)
+{
+	exit(0);
+	return (0);
+}
+
+
+int init_cube(void)
+{
+	t_game      game;
+	t_player    player;
+	int         i;
+
+	// Initialize key states
+	i = 0;
+	while (i < 65536)
+	{
+		game.keys[i] = 0;
+		i++;
+	}
+
+	// Initialize player
+	init_player(&player);
+
+	// Initialize MLX
+	game.mlx = mlx_init();
+	if (!game.mlx)
+		return (1);
+
+	game.mlx_window = mlx_new_window(game.mlx, WIN_WIDTH, WIN_HEIGHT, "Cub3D");
+	if (!game.mlx_window)
+		return (1);
+
+	// Create image
+	game.img.img = mlx_new_image(game.mlx, WIN_WIDTH, WIN_HEIGHT);
+	game.img.addr = mlx_get_data_addr(game.img.img, &game.img.bits_per_pixel, 
+				   &game.img.line_length, &game.img.endian);
+
+	game.player = &player;
+
+	// Set up event hooks
+	mlx_hook(game.mlx_window, 2, 1L<<0, key_press, &game);
+	mlx_hook(game.mlx_window, 3, 1L<<1, key_release, &game);
+	mlx_hook(game.mlx_window, 17, 0, close_window, &game);
+	mlx_loop_hook(game.mlx, game_loop, &game);
+
+	mlx_loop(game.mlx);
+	return (0);
+}
+
+char	**create_test_map(void)
+{
+	char **map = malloc(sizeof(char*) * 10);
+
+	map[0] = strdup("1111111111");
+	map[1] = strdup("1000000001");
+	map[2] = strdup("1000W00001");
+	map[3] = strdup("1000011001");
+	map[4] = strdup("1001000001");
+	map[5] = strdup("1000011001");
+	map[6] = strdup("1000100001");
+	map[7] = strdup("1000000001");
+	map[8] = strdup("1000000001");
+	map[9] = strdup("1111111111");
+
+	return (map);
+}
+
+int main()
+{
+	return (init_cube());
 }
