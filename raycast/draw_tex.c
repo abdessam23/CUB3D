2517fa9f1181f6ac58e7408faf13e2c36bd6b0a8
Abdellahsyani <syaniabdos@gmail.com>
2025-09-20 14:59:10 +0100

    fix stretched wall

--- raycast/draw_tex.c
+++ raycast/draw_tex.c
@@ -12,71 +12,159 @@
 
 #include "../cube.h"
 
-void	draw_W_E(t_game *game, t_player *player, int column)
+void draw_W_E(t_game *game, t_player *player, int column)
 {
-	int	texX;
-	int	texY; 
-	int	y;
+    int y = player->start_draw;
+    int texX, texY;
+    float step, texPos;
+    int texHeight;
+    unsigned char *texAddr;
+    int bpp, lineLen;
 
-	y = player->start_draw;
-	if (player->raydiX > 0)
-	{
-		texX = (int)(player->wallX * game->west_width);
-		if ((player->side == 0 && player->raydiX > 0) || (player->side == 1 && player->raydiY < 0))
-			texX = game->west_width - 1 - texX;
-		while (y < player->end_draw)
-		{
-			texY = (int)(((y - player->start_draw) * game->west_height) / player->line_height);
-			unsigned int color = *(unsigned int *)(game->west_addr + texY * game->west_line_len + texX * (game->west_bpp / 8));
-			put_pixel(&game->img, column, y, color);
-			y++;
-		}
-	}
-	else
-{
-		texX = (int)(player->wallX * game->east_width);
-		while (y < player->end_draw)
-		{
-			texY = (int)(((y - player->start_draw) * game->east_height) / player->line_height);
-			unsigned int color = *(unsigned int *)(game->east_addr + texY * game->east_line_len + texX * (game->east_bpp / 8));
-			put_pixel(&game->img, column, y, color);
-			y++;
-		}
-	}
+    if (player->raydiX > 0)
+    {
+        texX = (int)(player->wallX * game->west_width);
+        if ((player->side == 0 && player->raydiX > 0) || (player->side == 1 && player->raydiY < 0))
+            texX = game->west_width - 1 - texX;
+        texHeight = game->west_height;
+        texAddr = (unsigned char *)game->west_addr;
+        bpp = game->west_bpp / 8;
+        lineLen = game->west_line_len;
+    }
+    else
+    {
+        texX = (int)(player->wallX * game->east_width);
+        texHeight = game->east_height;
+        texAddr = (unsigned char *)game->east_addr;
+        bpp = game->east_bpp / 8;
+        lineLen = game->east_line_len;
+    }
+
+    step = (float)texHeight / player->line_height;
+    texPos = (player->start_draw - WIN_HEIGHT / 2 + player->line_height / 2) * step;
+
+    while (y < player->end_draw)
+    {
+        texY = (int)texPos;
+        if (texY >= texHeight) texY = texHeight - 1;
+        texPos += step;
+
+        unsigned int color = *(unsigned int *)(texAddr + texY * lineLen + texX * bpp);
+        put_pixel(&game->img, column, y, color);
+        y++;
+    }
 }
 
-void	draw_N_S(t_game *game, t_player *player, int column)
+void draw_N_S(t_game *game, t_player *player, int column)
 {
-	int	texX;
-	int	texY; 
-	int	y;
+    int y = player->start_draw;
+    int texX, texY;
+    float step, texPos;
+    int texHeight;
+    unsigned char *texAddr;
+    int bpp, lineLen;
 
-	y = player->start_draw;
-	if (player->raydiX > 0)
-	{
-		texX = (int)(player->wallX * game->north_width);
-		while (y < player->end_draw)
-		{
-			texY = (int)(((y - player->start_draw) * game->north_height) / player->line_height);
-			unsigned int color = *(unsigned int *)(game->north_addr + texY * game->north_line_len + texX * (game->north_bpp / 8));
-			put_pixel(&game->img, column, y, color);
-			y++;
-		}
-	}
-	else
-	{
-		texX = (int)(player->wallX * game->south_width);
-		if ((player->side == 0 && player->raydiX > 0) || (player->side == 1 && player->raydiY < 0))
-			texX = game->south_width - 1 - texX;
-		while (y < player->end_draw)
-		{
-			texY = (int)(((y - player->start_draw) * game->south_height) / player->line_height);
-			unsigned int color = *(unsigned int *)(game->south_addr + texY * game->south_line_len + texX * (game->south_bpp / 8));
-			put_pixel(&game->img, column, y, color);
-			y++;
-		}
-	}
+    if (player->raydiY > 0)
+    {
+        texX = (int)(player->wallX * game->north_width);
+        texHeight = game->north_height;
+        texAddr = (unsigned char *)game->north_addr;
+        bpp = game->north_bpp / 8;
+        lineLen = game->north_line_len;
+    }
+    else
+    {
+        texX = (int)(player->wallX * game->south_width);
+        if ((player->side == 0 && player->raydiX > 0) || (player->side == 1 && player->raydiY < 0))
+            texX = game->south_width - 1 - texX;
+        texHeight = game->south_height;
+        texAddr = (unsigned char *)game->south_addr;
+        bpp = game->south_bpp / 8;
+        lineLen = game->south_line_len;
+    }
+
+    step = (float)texHeight / player->line_height;
+    texPos = (player->start_draw - WIN_HEIGHT / 2 + player->line_height / 2) * step;
+
+    while (y < player->end_draw)
+    {
+        texY = (int)texPos;
+        if (texY >= texHeight) texY = texHeight - 1;
+        texPos += step;
+
+        unsigned int color = *(unsigned int *)(texAddr + texY * lineLen + texX * bpp);
+        put_pixel(&game->img, column, y, color);
+        y++;
+    }
 }
+
+
+
+/*void	draw_W_E(t_game *game, t_player *player, int column)*/
+/*{*/
+/*	int	texX;*/
+/*	int	texY; */
+/*	int	y;*/
+/**/
+/*	y = player->start_draw;*/
+/*	if (player->raydiX > 0)*/
+/*	{*/
+/*		texX = (int)(player->wallX * game->west_width);*/
+/*		if ((player->side == 0 && player->raydiX > 0) || (player->side == 1 && player->raydiY < 0))*/
+/*			texX = game->west_width - 1 - texX;*/
+/*		while (y < player->end_draw)*/
+/*		{*/
+/*			texY = (int)(((y - player->start_draw) * game->west_height) / player->line_height);*/
+/*			unsigned int color = *(unsigned int *)(game->west_addr + texY * game->west_line_len + texX * (game->west_bpp / 8));*/
+/*			put_pixel(&game->img, column, y, color);*/
+/*			y++;*/
+/*		}*/
+/*	}*/
+/*	else*/
+/*{*/
+/*		texX = (int)(player->wallX * game->east_width);*/
+/*		while (y < player->end_draw)*/
+/*		{*/
+/*			texY = (int)(((y - player->start_draw) * game->east_height) / player->line_height);*/
+/*			unsigned int color = *(unsigned int *)(game->east_addr + texY * game->east_line_len + texX * (game->east_bpp / 8));*/
+/*			put_pixel(&game->img, column, y, color);*/
+/*			y++;*/
+/*		}*/
+/*	}*/
+/*}*/
+/**/
+/*void	draw_N_S(t_game *game, t_player *player, int column)*/
+/*{*/
+/*	int	texX;*/
+/*	int	texY; */
+/*	int	y;*/
+/**/
+/*	y = player->start_draw;*/
+/*	if (player->raydiY > 0)*/
+/*	{*/
+/*		texX = (int)(player->wallX * game->north_width);*/
+/*		while (y < player->end_draw)*/
+/*		{*/
+/*			texY = (int)(((y - player->start_draw) * game->north_height) / player->line_height);*/
+/*			unsigned int color = *(unsigned int *)(game->north_addr + texY * game->north_line_len + texX * (game->north_bpp / 8));*/
+/*			put_pixel(&game->img, column, y, color);*/
+/*			y++;*/
+/*		}*/
+/*	}*/
+/*	else*/
+/*	{*/
+/*		texX = (int)(player->wallX * game->south_width);*/
+/*		if ((player->side == 0 && player->raydiX > 0) || (player->side == 1 && player->raydiY < 0))*/
+/*			texX = game->south_width - 1 - texX;*/
+/*		while (y < player->end_draw)*/
+/*		{*/
+/*			texY = (int)(((y - player->start_draw) * game->south_height) / player->line_height);*/
+/*			unsigned int color = *(unsigned int *)(game->south_addr + texY * game->south_line_len + texX * (game->south_bpp / 8));*/
+/*			put_pixel(&game->img, column, y, color);*/
+/*			y++;*/
+/*		}*/
+/*	}*/
+/*}*/
 
 void	Draw_textures(t_game *game, t_player *player, int column)
 {
@@ -96,8 +184,8 @@ int	to_hex(int r, int g, int b)
 void	draw_wall_column(t_game *game, t_player *player, int column)
 {
 	// get colors
-	int roof_color = to_hex(player->roof[0], player->roof[1], player->roof[2]);
-	int floor_color = to_hex(player->floor[0], player->floor[1], player->floor[2]);
+	/*int roof_color = to_hex(player->roof[0], player->roof[1], player->roof[2]);*/
+	/*int floor_color = to_hex(player->floor[0], player->floor[1], player->floor[2]);*/
 	// Calculate line height and draw positions
 	if (player->side == 0)
 		player->wallp = player->dsidX - player->dx;
